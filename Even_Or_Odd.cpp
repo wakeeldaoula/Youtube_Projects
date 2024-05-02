1435e19d2ea4efe83c122c851f5af466f66aa905
wakeeldaoula <149290107+wakeeldaoula@users.noreply.github.com>
2024-05-02 02:25:35 -0700

    Add files via upload

---
+++ Even_Or_Odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num;
+cout<<"Enter a number:";
+cin>>num;
+if(num%2==0){
+    cout<<num<<" is an even number\n";
+}
+else{
+    cout<<num<<" is an odd number\n";
+}
+    return 0;
+}
---
+++ Positive_Or_Negative.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num;
+cout<<"Enter a number:";
+cin>>num;
+if(num>=0){
+    cout<<num<<" is a positive number\n";
+}
+else{
+    cout<<num<<" is a negative number\n";
+}
+    return 0;
+}
